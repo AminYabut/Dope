d376d52894e06269c28ce95f9ad5adc2e8830849
Jon Biddle <91924009+coconutbird@users.noreply.github.com>
2022-04-19 03:44:46 +1200

    add ToSnakeCase helper

--- UnispectEx.Core/Util/Helpers.cs
+++ UnispectEx.Core/Util/Helpers.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-
+using System.Text;
 using dnlib.DotNet;
 
 using UnispectEx.Core.Mono;
 
 namespace UnispectEx.Core.Util {
     public static class Helpers {
-        public static IEnumerable<Tuple<TypeDef, MonoClass>> CorrelateClasses(IEnumerable<TypeDef> typeDefs, IEnumerable<MonoClass> monoClasses) {
+        public static IEnumerable<Tuple<TypeDef, MonoClass>> CorrelateClasses(
+            IEnumerable<TypeDef> typeDefs,
+            IEnumerable<MonoClass> monoClasses) {
             var l = typeDefs.OrderBy(x => x.MDToken.ToInt32());
             var r = monoClasses.OrderBy(x => x.Token);
 
@@ -19,7 +22,9 @@ namespace UnispectEx.Core.Util {
                 yield return new Tuple<TypeDef, MonoClass>(e1.Current, e2.Current);
         }
 
-        public static IEnumerable<Tuple<FieldDef, MonoClassField>> CorrelateFields(IEnumerable<FieldDef> fieldDefs, IEnumerable<MonoClassField> classFields) {
+        public static IEnumerable<Tuple<FieldDef, MonoClassField>> CorrelateFields(
+            IEnumerable<FieldDef> fieldDefs,
+            IEnumerable<MonoClassField> classFields) {
             var l = fieldDefs.OrderBy(x => x.MDToken.ToInt32());
             var r = classFields.OrderBy(x => x.Token);
 
@@ -28,6 +33,62 @@ namespace UnispectEx.Core.Util {
 
             while (e1.MoveNext() && e2.MoveNext())
                 yield return new Tuple<FieldDef, MonoClassField>(e1.Current, e2.Current);
+        }
+
+        // https://stackoverflow.com/a/67332992/17264463
+        public static string ToSnakeCase(string text) {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            var builder = new StringBuilder(text.Length + Math.Min(2, text.Length / 5));
+            var lastCategory = default(UnicodeCategory?);
+
+            for (var i = 0; i < text.Length; ++i) {
+                var l = text[i];
+                if (l == '_') {
+                    builder.Append('_');
+                    lastCategory = null;
+
+                    continue;
+                }
+
+                var category = char.GetUnicodeCategory(l);
+                switch (category) {
+                    case UnicodeCategory.UppercaseLetter:
+                    case UnicodeCategory.TitlecaseLetter:
+                        if (lastCategory == UnicodeCategory.SpaceSeparator ||
+                            lastCategory == UnicodeCategory.LowercaseLetter ||
+                            lastCategory != UnicodeCategory.DecimalDigitNumber &&
+                            lastCategory != null &&
+                            i > 0 &&
+                            i + 1 < text.Length &&
+                            char.IsLower(text[i + 1])) {
+                            builder.Append('_');
+                        }
+
+                        l = char.ToLower(l, CultureInfo.InvariantCulture);
+
+                        break;
+
+                    case UnicodeCategory.LowercaseLetter:
+                    case UnicodeCategory.DecimalDigitNumber:
+                        if (lastCategory == UnicodeCategory.SpaceSeparator)
+                            builder.Append('_');
+
+                        break;
+
+                    default:
+                        if (lastCategory is not null)
+                            lastCategory = UnicodeCategory.SpaceSeparator;
+
+                        continue;
+                }
+
+                builder.Append(l);
+                lastCategory = category;
+            }
+
+            return builder.ToString();
         }
     }
 }
