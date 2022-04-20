209ae053aa23cc16f8d9df05ac977896086f198f
Jon Biddle <91924009+coconutbird@users.noreply.github.com>
2022-04-21 04:09:40 +1200

    add plugin support.

--- UnispectEx/Program.cs
+++ UnispectEx/Program.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
-using System.Text;
-
+using System.Reflection;
 using dnlib.DotNet;
 
+using EscapeFromTarkov;
+using UnispectEx.Core;
 using UnispectEx.Core.Inspector;
 using UnispectEx.Core.Util;
 
 namespace UnispectEx {
     internal static class Program {
+        private static IEnumerable<IPlugin> GetPlugins() {
+            var pluginDirectory = Path.Join(AppContext.BaseDirectory, "plugins");
+
+            if (!Directory.Exists(pluginDirectory))
+                Directory.CreateDirectory(pluginDirectory);
+
+            foreach (var file in Directory.GetFiles(pluginDirectory, "*.dll")) {
+                var assembly = Assembly.LoadFile(file);
+
+                foreach (var pluginType in assembly.GetTypes().Where(typeof(IPlugin).IsAssignableFrom))
+                    yield return (IPlugin) Activator.CreateInstance(pluginType)!;
+            }
+        }
+
         private static bool ProcessAndSerialize(ImmutableList<MetadataContainer> containers, string outputDirectory) {
             var processor = new DefaultDumpProcessor();
             if (!processor.Initialize(containers))
@@ -24,15 +39,16 @@ namespace UnispectEx {
         }
 
         internal static void Main(string[] args) {
-            if (args.Length != 2) {
+            if (args.Length < 2) {
                 Console.WriteLine("*** https://github.com/coconutbird/UnispectEx ***");
-                Console.WriteLine("[*] UnispectEx <process> <dll>");
+                Console.WriteLine("[*] UnispectEx <process> <dll> <plugin:optional>");
 
                 return;
             }
 
             var processName = args[0];
             var dllName = args[1];
+            var pluginName = args.Length == 3 ? args[2] : null;
 
             var memory = new LocalMemory();
 
@@ -64,7 +80,7 @@ namespace UnispectEx {
             module.Context = moduleContext;
 
             assemblyResolver.AddToCache(module);
-            
+
             assemblyResolver.PreSearchPaths.Add(managedDirectory);
 
             foreach (var reference in module.GetAssemblyRefs()) {
@@ -82,7 +98,23 @@ namespace UnispectEx {
             var correlatedClasses = Helpers.CorrelateClasses(module.GetTypes(), assembly.Image.Types());
             var containers = correlatedClasses.Select(x => new MetadataContainer(x.Item1, x.Item2)).ToImmutableList();
 
-            var analyzer = new DefaultDumpAnalyzer();
+            IDumpAnalyzer? analyzer;
+            if (pluginName is not null) {
+                var plugin = GetPlugins().FirstOrDefault(x => x.Name == pluginName);
+
+                if (plugin is null)
+                    return;
+
+                analyzer = plugin.DumpAnalyzers.FirstOrDefault();
+
+                if (analyzer is null) {
+                    Console.WriteLine("[-] invalid plugin name!");
+
+                    return;
+                }
+            } else {
+                analyzer = new DefaultDumpAnalyzer();
+            }
 
             if (!analyzer.Analyze(containers)) {
                 Console.WriteLine("[-] Analysis failed!");
