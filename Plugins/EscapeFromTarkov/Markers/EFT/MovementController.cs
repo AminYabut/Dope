799ee19d48ceebd8d09174d6d23b41dafca8eb8d
Jon Biddle <91924009+coconutbird@users.noreply.github.com>
2022-05-03 02:27:24 +1200

    add CurrentViewAngles

--- Plugins/EscapeFromTarkov/Markers/EFT/MovementController.cs
+++ Plugins/EscapeFromTarkov/Markers/EFT/MovementController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Immutable;
 using dnlib.DotNet;
+using dnlib.DotNet.Emit;
 using EscapeFromTarkov.Extensions;
 using UnispectEx.Core.Inspector;
 
@@ -23,6 +24,12 @@ internal class MovementController : IMarker {
         if (movementContextControllerContainer is null)
             return false;
 
+        var viewAngleFieldDef = FindViewAngleFieldDef(type);
+        if (viewAngleFieldDef is null)
+            return false;
+
+        viewAngleFieldDef.Name = "_currentViewAngles";
+
         type.Namespace = "EFT";
         type.Name = "MovementController";
 
@@ -30,5 +37,36 @@ internal class MovementController : IMarker {
         movementContextControllerContainer.ExportNonObfuscatedSymbols();
 
         return true;
+    }
+
+    private FieldDef? FindViewAngleFieldDef(TypeDef movementController) {
+        var method = movementController.FindMethod("RestorePreviousYaw");
+        if (method is null)
+            return null;
+
+        if (!method.HasBody)
+            return null;
+
+        var instructions = method.Body.Instructions;
+        for (var i = 0; i < instructions.Count - 3; ++i) {
+            if (instructions[i].OpCode != OpCodes.Ldflda ||
+                instructions[i + 1].OpCode != OpCodes.Ldarg_0 ||
+                instructions[i + 2].OpCode != OpCodes.Ldflda ||
+                instructions[i + 3].OpCode != OpCodes.Ldfld ||
+                instructions[i + 4].OpCode != OpCodes.Stfld)
+                continue;
+
+            if (OpCodes.Ldflda == OpCodes.Stfld)
+                return null;
+
+            var field = instructions[i].Operand as FieldDef;
+            if (field is null)
+                continue;
+
+            if (field.FieldType.ScopeType.ToString() == "UnityEngine.Vector2")
+                return field;
+        }
+
+        return null;
     }
 }
